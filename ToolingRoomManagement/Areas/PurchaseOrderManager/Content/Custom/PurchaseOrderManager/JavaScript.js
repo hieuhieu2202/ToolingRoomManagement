09fdd3ef940d091f078c21bd7b8f30af0e6eb5f8
HoangTien <HoangTien@DESKTOP-8CQEP1V>
2023-07-05 17:36:46 +0700

    add from purchase request

---
+++ ToolingRoomManagement/Areas/PurchaseOrderManager/Content/Custom/PurchaseOrderManager/JavaScript.js
@@ -0,0 +1,41 @@
+﻿var table_employees;
+var buttonNewRequest;
+
+$(document).ready(function () {
+    buttonNewRequest = initConfettiButton('buttonNewRequest', 'canvasNewRequest');
+
+    InitTable(); 
+});
+
+function InitTable() {
+    var options = {
+        scrollY: '75vh',
+        paging: false,
+        info: false,
+        searching: false,
+        scrollCollapse: true,
+        fixedHeader: {
+            header: true,
+        },    
+        columnDefs: [
+            { targets: [6], orderable: false },
+            { targets: "_all", orderable: true },
+        ],
+        columns: [
+            { className: "" },
+            { className: "" },
+            { className: "" },
+            { className: "" },
+            { className: "" },
+            { className: "" },
+            { className: "" },
+
+        ],
+    }
+    table_employees = new DataTable('#table_employees', options);
+
+    $('#table_employees_search').click(function () {
+        table_employees.search($(this).val()).draw();
+    });
+
+}
